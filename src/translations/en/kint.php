506f7bc51c2c93b6e11f42ecfc0f33f4a491322e
Josh Crawford <joshua@sgroup.com.au>
2024-05-27 14:52:26 +1000

    Update English translations.

--- src/translations/en/kint.php
+++ src/translations/en/kint.php
@@ -1,4 +1,11 @@
 <?php
 
 return [
+  'Aante Light' => 'Aante Light',
+  'Display Theme' => 'Display Theme',
+  'Kint' => 'Kint',
+  'Original' => 'Original',
+  'Pick a theme for Kint to use.' => 'Pick a theme for Kint to use.',
+  'Solarized' => 'Solarized',
+  'Solarized Dark' => 'Solarized Dark',
 ];
